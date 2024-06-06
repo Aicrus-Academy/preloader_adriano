9dba5656555bf5714e8b6e4fafef793aec538400
Grupo Aicrus <93139175+Aicrus@users.noreply.github.com>
2024-06-06 19:22:59 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,55 +1,48 @@
-<script src="https://cdn.jsdelivr.net/npm/lottie-web@5.7.3/build/player/lottie.min.js"></script>
-<script type="application/javascript" src="https://aicrus.github.io/FlutterFlow_preloader/preloader.js" defer></script>
+const overlay = document.createElement('div');
+overlay.id = 'loader';
+overlay.style.cssText = `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 999;
+`;
 
-<script type="application/javascript">
-    const overlay = document.createElement('div');
-    overlay.id = 'loader';
-    overlay.style.cssText = `
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(255, 255, 255, 0.7);
-        z-index: 999;
-    `;
+const lottieContainer = document.createElement('div');
+lottieContainer.id = 'lottieContainer';
+lottieContainer.style.cssText = `
+    width: 200px;
+    height: 200px;
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
 
-    const lottieContainer = document.createElement('div');
-    lottieContainer.id = 'lottieContainer';
-    lottieContainer.style.cssText = `
-        max-width: 100%;
-        max-height: 100%;
-        display: none;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    `;
-    lottieContainer.style.display = 'none';
+document.body.appendChild(overlay);
+document.body.appendChild(lottieContainer);
 
-    document.body.appendChild(overlay);
-    document.body.appendChild(lottieContainer);
+function hideOverlay() {
+    overlay.style.display = 'none';
+    lottieContainer.style.display = 'block';
+    // Substitua pela URL direta do seu arquivo JSON do Lottie.
+    lottie.loadAnimation({
+        container: lottieContainer,
+        renderer: 'svg', // ou 'canvas' se preferir
+        loop: true,
+        autoplay: true,
+        path: 'https://aicrus.github.io/FlutterFlow_preloader/LoadAicrusComponents.json',
+    });
+}
 
-    let animationLoaded = false;
+document.addEventListener('DOMContentLoaded', () => {
+    hideOverlay(); // Teste sem o setTimeout
+});
 
-    function hideOverlay() {
-        if (!animationLoaded) {
-            overlay.style.display = 'none';
-            lottieContainer.style.display = 'block';
-            // Substitua pela URL direta do seu arquivo JSON do Lottie.
-            lottie.loadAnimation({
-                container: lottieContainer,
-                renderer: 'svg', // ou 'canvas' se preferir
-                loop: true,
-                autoplay: true,
-                path: 'https://aicrus.github.io/FlutterFlow_preloader/LoadAicrusComponents.json',
-            });
-            animationLoaded = true;
-        }
-    }
-
-    window.addEventListener('load', hideOverlay);
-</script>
+window.addEventListener('load', hideOverlay);
