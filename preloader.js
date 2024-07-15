be6444c6daee4be4a1f6bfa1940f6be9dc841d81
Grupo Aicrus <93139175+Aicrus@users.noreply.github.com>
2024-07-14 21:41:07 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,49 +1,27 @@
-const overlay = document.createElement('div');
-overlay.id = 'loader';
-overlay.style.cssText = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 10%;
-    height: 10%;
-    background-color: rgba(255, 255, 255, 0.7);
-    z-index: 999;
-`;
+const overlay = document.createElement('div')
+overlay.id = 'loader'
+overlay.style.cssText = 'display:flex;justify-content:center;align-items:center;position:fixed;top:0;left:0;width:10%;height:10%;background-color:rgba(255,255,255,0.7);z-index:999;'
 
-const lottieContainer = document.createElement('div');
-lottieContainer.id = 'lottieContainer';
-lottieContainer.style.cssText = `
-    max-width: 10%;
-    max-height: 10%;
-    display: none;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-90%, -90%);
-`;
-lottieContainer.style.display = 'none';
+const lottieContainer = document.createElement('div')
+lottieContainer.id = 'lottieContainer'
+lottieContainer.style.cssText = 'max-width:10%;max-height:10%;display:none;position:absolute;top:50%;left:50%;transform:translate(-90%,-90%);'
+lottieContainer.style.display = 'none'
 
-document.body.appendChild(overlay);
-document.body.appendChild(lottieContainer);
+document.body.appendChild(overlay)
+document.body.appendChild(lottieContainer)
 
 function hideOverlay() {
-    overlay.style.display = 'none';
-    lottieContainer.style.display = 'block';
-    // Substitua pela URL direta do seu arquivo JSON do Lottie.
+    overlay.style.display = 'none'
+    lottieContainer.style.display = 'block'
     lottie.loadAnimation({
         container: lottieContainer,
-        renderer: 'svg', // ou 'canvas' se preferir
+        renderer: 'svg',
         loop: true,
         autoplay: true,
         path: 'https://aicrus-academy.github.io/preloader_adriano/Animation.json',
-    });
+    })
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    hideOverlay(); // Teste sem o setTimeout
-});
+document.addEventListener('DOMContentLoaded', () => hideOverlay())
 
-window.addEventListener('load');
+window.addEventListener('load')
